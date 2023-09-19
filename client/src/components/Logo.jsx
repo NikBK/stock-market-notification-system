d2a33a4a69704c495ccaa063c9a8711e44998ab9
NikBK <nikhilbalakrishna5@gmail.com>
2023-09-19 16:15:07 +0530

    Logo image path updated

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,7 +2,7 @@ const Logo = () => {
     return (
         <img
             alt="app-logo"
-            src="/assets/images/app_logo.png"
+            src="./assets/images/app_logo.png"
             className="w-8 h-8 self-center hover:cursor-pointer"
         />
     )
